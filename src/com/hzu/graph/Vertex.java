aa9ddf84994439a3eb1b22cc042daaff5e05b4a0
golden retriever <1146726774@qq.com>
2019-09-05 19:30:20 +0800

    add Graph

---
+++ src/com/hzu/graph/Vertex.java
@@ -0,0 +1,30 @@
+package com.hzu.graph;
+
+/** 图的顶点
+ * @author zhanghuihong
+ * @since 2019-09-01
+ */
+public class Vertex {
+
+    //顶点的值
+    private String value;
+    //遍历时标识是否被访问过
+    public boolean visited;
+
+    public Vertex(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
