e522426099e28c5996cac0268727eb82272b2425
golden retriever <1146726774@qq.com>
2020-08-04 23:16:41 +0800

    Create _0048_RotateImage.java

---
+++ src/com/pianocello/leetcode/_0048_RotateImage.java
@@ -0,0 +1,61 @@
+package com.pianocello.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * 将图像顺时针旋转 90 度。
+ * 说明：
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ *
+ * @author PianoCello
+ * @date 2020-08-04
+ */
+public class _0048_RotateImage {
+
+    public static void rotate(int[][] matrix) {
+        // 先对角线交换
+        int len = matrix.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // 再左右交换
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][len - j - 1];
+                matrix[i][len - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+}
